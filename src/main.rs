68c73cf2d87c039d008a2ccef423eaa85f4def43
B.Billion <buhebillion@gmail.com>
2025-03-23 00:59:53 +0200

    almighty

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(non_snake_case)]
+
+fn main
+()
+{
+    
+}
+
+use trpl::Html;
+
+async fn pageTitle
+(url: &str) -> Option<String>
+{
+    let response = trpl::get(url).await;
+    let responseText = response.text().await;
+    Html::parse(&responseText)
+    .select_first("title")
+    .map(|titleElement| titleElement.inner_html())
 }
