bc7a8e86e679df73910a1ad7ae1d155e566539e9
B.Billion <buhebillion@gmail.com>
2025-03-23 01:04:25 +0200

    almighty push

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,18 @@
 fn main
 ()
 {
-    
+
 }
 
 use trpl::Html;
 
 async fn pageTitle
 (url: &str) -> Option<String>
-{
+{   /*
     let response = trpl::get(url).await;
     let responseText = response.text().await;
+    */
+    let responseText = trpl::get(url).await.text().await;
     Html::parse(&responseText)
     .select_first("title")
     .map(|titleElement| titleElement.inner_html())
